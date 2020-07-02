eb57e81bfbef8574b4f1ac296f171db47cc04fe9
Nisharu <39903426+Nisharu@users.noreply.github.com>
2020-07-03 00:26:34 +0300

    Healthbars + Mech prefab

--- Assets/Scripts/General Scripts/Menu & UI/Healthbar.cs
+++ Assets/Scripts/General Scripts/Menu & UI/Healthbar.cs
@@ -1,18 +1,40 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Healthbar : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private Image _healthbar;
+    [SerializeField] private float _updateSpeed = 0.1f;
+
+    void Awake()
     {
-        
+        GetComponentInParent<Health>().OnHealthChanged += HealthChange;
     }
 
+    private void HealthChange(float pct)
+    {
+        StartCoroutine(ChangeHealthbar(pct));
+    }
+    private IEnumerator ChangeHealthbar(float pct)
+    {
+        float HealthbarBefore = _healthbar.fillAmount;
+        float elapsed = 0f;
+
+        while (elapsed < _updateSpeed)
+        {
+            elapsed += Time.deltaTime;
+            _healthbar.fillAmount = Mathf.Lerp(HealthbarBefore, pct, elapsed / _updateSpeed);
+            yield return null;
+        }
+
+        _healthbar.fillAmount = pct;
+    }
     // Update is called once per frame
     void Update()
     {
-        
+        transform.LookAt(Camera.main.transform);
+        transform.Rotate(0, 180, 0);
     }
 }
